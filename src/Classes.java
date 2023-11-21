c82fc718c6fd693d7f2d74605adc91eda6920cf7
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-11-21 12:09:04 +0100

    Classes explanation

--- src/Classes.java
+++ src/Classes.java
@@ -1,9 +1,22 @@
 public class Classes {
 
-    class Car {
+    static class Car {
         public int hp;
         public String model;
 
+        Car (int hp, String model) {
+            this.hp = hp;
+            this.model = model;
+        }
 
+        public void allInfo() {
+            System.out.println("El coche " + this.model + " tiene " + this.hp + " caballos de fuerza");
+        }
+    }
+
+    public static void main(String[] args) {
+        // to create an instance of a class, we have to make it on a "main"
+        Car coche1 = new Car(1000, "Tesla");
+        coche1.allInfo();
     }
 }
