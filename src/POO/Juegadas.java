045ae5a2d14845b44b7ad5eb21b74b7f5ed12db4
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-11-26 11:39:29 +0100

    POO

---
+++ src/POO/Juegadas.java
@@ -0,0 +1,11 @@
+package POO;
+
+public interface Juegadas {
+    public static char posicionamiento() {
+        return 0;
+    }
+
+    public static String union() {
+        return "";
+    }
+}
--- src/ejercicios/Array_EJ.java
+++ src/ejercicios/Array_EJ.java
@@ -1,11 +1,28 @@
 package ejercicios;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Array_EJ {
+    // CREACIÓN DE CONSTANTES
+    private static final String BIENVENIDA = "Bienvenido a tu calculadora";
+    private static final String INTRODUCE_PRIMER_NUMERO = "Introduce el primer número";
+    private static final String INTRODUCE_SEGUNDO_NUMERO = "Introduce el segundo número";
+    private static final String RESULTADO = "La suma del primer y segundo número es: ";
 
+    // MAIN -> TODO LO QUE SE PINTARÁ
     public static void main(String[] args) {
-        int[] array = new int[]{1, 2, 3};
-        System.out.println(array[1]);
+        System.out.println(BIENVENIDA);
+        System.out.println(INTRODUCE_PRIMER_NUMERO);
+        Scanner s = new Scanner(System.in);
+        int primer_numero = s.nextInt();
+        System.out.println(INTRODUCE_SEGUNDO_NUMERO);
+        int segundo_numero = s.nextInt();
+        suma(primer_numero, segundo_numero);
+    }
+
+    // MÉTODOS
+    private static void suma(int a, int b) {
+        System.out.println(RESULTADO + (a + b));
     }
 }
